4072185baddddba7e0eba0d0fea2461d81090749
John Lyons <johnlyonsdev@gmail.com>
2021-09-20 16:40:07 -0500

    Added remainder function

--- calc.js
+++ calc.js
@@ -53,6 +53,11 @@ reader.question("What would you like to calculate?", function(input){
 		let power = Math.pow(num1, num2)
 		console.log(power)
 	}
+
+	if (mathSymbol === "%") {
+		console.log(num1 % num2)
+	}
+
 	// This line closes the connection to the command line interface.
 	reader.close()
 
